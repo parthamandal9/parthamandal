52e8a65b04fe031cdd5001d5d089dd4a0ec87c3b
parthamandal9 <75560742+parthamandal9@users.noreply.github.com>
2020-12-18 01:16:24 +0530

    Add files via upload

---
+++ Mdbootstrap/js/main.js
@@ -0,0 +1,56 @@
+const typedTextSpan = document.querySelector(".typed-text");
+            const cursorSpan = document.querySelector(".cursor");
+
+            const textArray = ["Freelancer", "Developer", "Database Designer"];
+            const typingDelay = 200;
+            const erasingDelay = 100;
+            const newTextDelay = 500;
+            let textArrayIndex = 0;
+            let charIndex = 0;
+
+            function type(){
+                if(charIndex < textArray[textArrayIndex].length){
+                    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+                    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+                    charIndex++;
+                    setTimeout(type, typingDelay);
+
+                }else{
+                    cursorSpan.classList.remove("typing");
+                    setTimeout(erase, newTextDelay);
+                }
+            }
+            function erase(){
+                if(charIndex > 0){
+                    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+                    typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+                    charIndex--;
+                    setTimeout(erase, erasingDelay);
+                }
+                else{
+                    cursorSpan.classList.remove("typing");
+                   textArrayIndex++;
+                   if(textArrayIndex >= textArray.length) textArrayIndex = 0;
+                   setTimeout(type, typingDelay + 500);
+                }
+            }
+            document.addEventListener("DOMContentLoaded", function(){
+                if(textArray.length)setTimeout(type, newTextDelay + 100);
+            });
+
+
+            
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
